0a135da0bf8a30292edde9629f7bc0a6714cf5f5
Lewis Jones <apple@apples-macbook.home>
2016-11-10 16:22:51 +0000

    added files for How Many Fingers

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func guess(sender: AnyObject) {
+        
+        let diceRoll = String(arc4random_uniform(6))
+        
+        if diceRoll == userGuessTextField.text {
+            
+            resultLabel.text = "You are right!"
+            
+        } else {
+        
+        resultLabel.text = "Wrong! It was a " + diceRoll
+        
+    }
+        
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
